fce6c8ef4bd44d80d0ae34061a696cd412bb57f9
Jiyin Zhang <zhangjiyin2023@ucla.edu>
2024-05-03 13:32:16 -0700

    done with app.js

--- public/app.js
+++ public/app.js
@@ -4,10 +4,21 @@ import Mover from "./mover.js";
 export default class App {
   constructor() {
     //TODO
+    // Find the TODO column
+    this.todoCol = document.getElementById("todo");
   }
 
   addCard(col, title, color) {
     //TODO
+    // Create a new card
+    const newCard = new Card(title, color);
+
+    // Find the corresponding column
+    const colElement = document.getElementById(col);
+
+    // Add the card to the column
+    //newCard.addToCol(col, new Mover());
+    newCard.addToCol(colElement);
   }
 
   //TODO
